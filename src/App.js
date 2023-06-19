0c601f5903be84ea1d0c95feab7b4eae2eb7f33f
Hassanalisuleiman <hassanalisuleiman49@gmail.com>
2023-06-19 21:09:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+      <div class="container">
+      <div class="login">
+        <form>
+          <h1>Login In</h1>
+          <hr />
+          <p>Explore the World!</p>
+          <label>Email</label>
+          <input type="text" placeholder="Email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+          <button><span>Submit</span></button>
+          <p><a href="#">Forgot Password?</a></p>
+        <closeform></closeform></form>
+      </div>
+      <div class="pic">
+        <img src="/pic/Meeting.png" />
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/components/login.js
@@ -0,0 +1,13 @@
+
+
+function login(){
+    return(
+      <div className="container">
+
+        <h1>hellow</h1>
+
+      </div>
+       
+    )
+}
+export default login;
